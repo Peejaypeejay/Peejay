4ff8dfefa3eff0ba2255c913e79b663163289592
Peejaypeejay <peejaypee00@gmail.com>
2024-05-25 14:47:26 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
+
+function clickLike1(){
+  let totalLike1 = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLike1.toString()
+}
+btnLike1.addEventListener("click",clickLike1)
