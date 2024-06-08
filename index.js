8c125536c261d95f3fc5516202e78e8ef6702ac7
Peejaypeejay <peejaypee00@gmail.com>
2024-06-08 14:16:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const countLike1 = document.getElementById("countLike1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLike2 = document.getElementById("countLike2")
 
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}
+submit.addEventListener("click", submitComment)
+
 function clickLike1(){
   let totalLike1 = parseInt(countLike1.value) + 1
   countLike1.textContent = totalLike1.toString()
@@ -14,3 +22,4 @@ function clickLike2(){
 }
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
+
